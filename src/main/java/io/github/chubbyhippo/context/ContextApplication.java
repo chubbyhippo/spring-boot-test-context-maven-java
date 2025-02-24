b4bd126d5ba1e54d38d37d3e69d2c7bb8838b1e1
chubbyhippo <mkmsacc@outlook.com>
2025-02-24 19:56:31 +0700

    Add initial Spring Boot project setup
    Initialized a new Spring Boot project with Maven, including its wrapper scripts, configuration files, and a simple application structure. Added essential dependencies for web, security, actuator, and testing, along with `.gitignore` and `.gitattributes` for version control.

---
+++ src/main/java/io/github/chubbyhippo/context/ContextApplication.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextApplication.class, args);
+	}
+
+}
